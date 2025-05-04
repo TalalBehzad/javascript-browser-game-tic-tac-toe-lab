3ca50e330d67298ef606573da4dee0f8a139b367
TalalBehzad <talalhaniali@gmail.com>
2025-05-04 19:23:41 +0300

    onstepfour

---
+++ js/app.js
@@ -0,0 +1,60 @@
+/*-------------------------------- Constants --------------------------------*/
+
+const squareEls = document.querySelector(`board`);
+const messageEls = document.querySelector(`message`)
+/*---------------------------- Variables (state) ----------------------------*/
+let board = ['', '', '', '', '', '', '', '', ''];
+let turn = `X`;
+let winner = `false`;
+let tie = `false`;
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+//function is a section of reusable code// 
+function init(){
+    console.log(`init`);
+    console.log(`render`);
+}
+init();
+
+function render(){
+
+}
+
+function updateBoard(){
+    board.forEach(updateBoard);
+}
+
+function updateMessage(){
+ if(winner and tie )  
+}
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
+
+
+
+
+
+
+
+
+//1) Define the required variables used to track the state of the game.
+
+//2) Store cached element references.
+
+//3) Upon loading, the game state should be initialized, and a function should 
+//   be called to render this game state.
+
+//4) The state of the game should be rendered to the user.
+
+//5) Define the required constants.
+
+//6) Handle a player clicking a square with a `handleClick` function.
+
+//7) Create Reset functionality
